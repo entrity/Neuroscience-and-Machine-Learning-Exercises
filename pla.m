5fcbfca204688bfb1522ae28d330febd639f49ba
Markham <quinesquines@gmail.com>
2016-09-26 17:15:48 -0700

    Include data for troubleshooting perceptron learning algorithm

--- pla.m
+++ pla.m
@@ -1,4 +1,4 @@
-N = 20;
+global N = 20;
 
 function r = myrand()
 	n = 50;
@@ -6,31 +6,38 @@ function r = myrand()
 end
 
 function s = mysign(val)
-	sign(val) + (0 == val)
+	s = sign(val) + (0 == val);
 end
 
 % Weights
-w = zeros(1,3)';
+global w = zeros(1,3)';
 % base and 2 inputs (for 20 data points)
-x = [
+global x = [
 		ones(1,N);
 		arrayfun(@ myrand, 1:N);
 		arrayfun(@ myrand, 1:N);
 	];
-% disp(x);
 
 % Line
 global m = myrand
 global b = myrand
-y = [];
+global y = [];
 for i = 1:N
 	y(i) = x(3,i) >= x(2,i)*m + b;
 end
 y(find(y == 0)) = -1;
-% disp(y)
 
-function render(x, w, h)
-	global m b
+% Hypothesis, i.e. w'*x
+function o = h(i)
+	global w x
+	o = mysign(w' * x);
+	if exist('i', 'var')
+		o = o(i);
+	end
+end
+
+function render()
+	global m b N x w
 	h = w' * x;
 	clf('reset')
 	hold on
@@ -42,22 +49,44 @@ function render(x, w, h)
 	px = linspace(-50,50);
 	py = px * m + b;
 	plot(px, py);
-	hold off
 end
 
+function mm = find_mismatch()
+	global w x N y
+	for i = 1:N
+		if y(i) ~= h(i)
+			mm = i;
+			return
+		end
+	end
+	mm = 0;
+end
 
 % Find mismatch
+iterations = 0;
 for i = 1:N
-	% Hypothesis (w'*x)
-	h = w' * x;
-	if y(i) ~= h(i)
+	mm = find_mismatch;
+	if mm
+		iterations += 1;
 		% printf('%2d mm %2d %2d\n', i, y(i), h(i))
 		w = w + y(i) * x(:,i);
-		render(x, w, h)
+		render
 		pause(0.07)
 		% disp(w');
 	end
 end
 
-render(x, w, h)
+m
+b
+iterations
+render
 w
+
+if find_mismatch
+	disp 'MISMATCH STILL EXISTS!!!!'
+	save('dump', 'x', 'y', 'h', 'w', 'm', 'b', 'iterations')
+	pause(60)
+	input('press enter ')
+else
+	pause(2)
+end
